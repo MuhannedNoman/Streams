ab064ee3f8ab37118beaf3d18d61700b32438e63
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-01-28 21:49:23 +0300

    Read and write to another file

--- App.js
+++ App.js
@@ -58,7 +58,7 @@
 
  const fs = require('fs');
  const readStream = fs.createReadStream('./example.txt','utf8');
-
+ const writeStream = fs.createWriteStream('example-2.txt');
  readStream.on('data',(chunk)=>{
-    console.log(chunk);
+    writeStream.write(chunk);
  });
