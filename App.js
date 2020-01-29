d8c1f21ee5dc755ba5cfe6419e860b5f12d6bb0a
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-01-29 06:45:48 +0300

    Initalize repo for piep

--- App.js
+++ App.js
@@ -59,6 +59,10 @@
  const fs = require('fs');
  const readStream = fs.createReadStream('./example.txt','utf8');
  const writeStream = fs.createWriteStream('example-2.txt');
- readStream.on('data',(chunk)=>{
-    writeStream.write(chunk);
- });
+ // Using Emitters
+//  readStream.on('data',(chunk)=>{
+//     writeStream.write(chunk);
+//  });
+
+// Using pipe
+readStream.pipe(writeStream);
